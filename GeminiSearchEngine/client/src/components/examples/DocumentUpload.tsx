3ada4d7d259dd570a3e3e3920bfadba8ed5f0eab
Pondara Sandeep <sandeep1932005@gmail.com>
2025-10-24 13:47:38 +0530

    Delete GeminiSearchEngine directory

--- GeminiSearchEngine/client/src/components/examples/DocumentUpload.tsx
+++
@@ -1,9 +0,0 @@
-import DocumentUpload from '../DocumentUpload'
-
-export default function DocumentUploadExample() {
-  return (
-    <DocumentUpload
-      onFileSelect={(files) => console.log('Files selected:', files)}
-    />
-  )
-}
--- GeminiSearchEngine/client/src/components/examples/SearchBar.tsx
+++
@@ -1,9 +0,0 @@
-import SearchBar from '../SearchBar'
-
-export default function SearchBarExample() {
-  return (
-    <SearchBar 
-      onSearch={(query) => console.log('Search query:', query)} 
-    />
-  )
-}
